39b06eb05499f71591185a252a791ebeeb9fd54c
iiandmy <frezhgrand@gmail.com>
2022-11-15 23:07:05 +0300

    Initial commit

---
+++ src/main/java/by/bsuir/fooddelivio/FoodDelivioApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.fooddelivio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDelivioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDelivioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/bsuir/fooddelivio/FoodDelivioApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.fooddelivio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDelivioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
