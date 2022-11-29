5408a65c93dfa8cc0a81c012c0131a8d6618e12d
iia.m.y <frezhgrand@gmail.com>
2022-11-30 01:02:29 +0300

    update application properties and created password encoder bean

--- src/main/java/by/bsuir/fooddelivio/FoodDelivioApplication.java
+++ src/main/java/by/bsuir/fooddelivio/FoodDelivioApplication.java
@@ -2,6 +2,10 @@ package by.bsuir.fooddelivio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class FoodDelivioApplication {
@@ -10,4 +14,8 @@ public class FoodDelivioApplication {
         SpringApplication.run(FoodDelivioApplication.class, args);
     }
 
+    @Bean
+    public NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
 }
