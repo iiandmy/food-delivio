323b04db018636bd84d9abb9b81087b77d6fb696
iia.m.y <frezhgrand@gmail.com>
2022-12-03 04:59:52 +0300

    add methods to search menus and dishes via id and foreign key

--- src/main/java/by/bsuir/fooddelivio/repository/DishRepository.java
+++ src/main/java/by/bsuir/fooddelivio/repository/DishRepository.java
@@ -1,7 +1,18 @@
 package by.bsuir.fooddelivio.repository;
 
 import by.bsuir.fooddelivio.entity.Dish;
+import by.bsuir.fooddelivio.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 public interface DishRepository extends JpaRepository<Dish, Long> {
+    @Transactional
+    @Query(
+            value = "SELECT * FROM dishes WHERE name = ?1 and menu_id = ?2",
+            nativeQuery = true
+    )
+    Optional<Dish> findByNameAndMenuId(String name, Long menuId);
 }
--- src/main/java/by/bsuir/fooddelivio/repository/MenuRepository.java
+++ src/main/java/by/bsuir/fooddelivio/repository/MenuRepository.java
@@ -2,6 +2,16 @@ package by.bsuir.fooddelivio.repository;
 
 import by.bsuir.fooddelivio.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    @Transactional
+    @Query(
+            value = "SELECT * FROM menus WHERE name = ?1 and restaurant_id = ?2",
+            nativeQuery = true
+    )
+    Optional<Menu> findByNameAndRestaurantId(String name, Long restaurantId);
 }
