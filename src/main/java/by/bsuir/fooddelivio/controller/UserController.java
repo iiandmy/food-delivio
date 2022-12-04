14872f4b42e522e76637e88653a25dc6fada941d
iia.m.y <frezhgrand@gmail.com>
2022-12-05 00:10:07 +0300

    add methods for user reviews and orders

--- src/main/java/by/bsuir/fooddelivio/controller/UserController.java
+++ src/main/java/by/bsuir/fooddelivio/controller/UserController.java
@@ -1,23 +1,44 @@
 package by.bsuir.fooddelivio.controller;
 
-import by.bsuir.fooddelivio.dto.LoginUserDto;
-import by.bsuir.fooddelivio.dto.RegisterUserDto;
+import by.bsuir.fooddelivio.config.JwtTokenUtil;
+import by.bsuir.fooddelivio.dto.order.OrderResponseDto;
 import by.bsuir.fooddelivio.entity.User;
-import by.bsuir.fooddelivio.exception.UserAlreadyExistsException;
-import by.bsuir.fooddelivio.exception.UserDoesntExistsException;
-import by.bsuir.fooddelivio.exception.UserWrongPasswordException;
+import by.bsuir.fooddelivio.mapper.OrderMapper;
+import by.bsuir.fooddelivio.service.OrderService;
 import by.bsuir.fooddelivio.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @CrossOrigin
-@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final OrderService orderService;
+    private final JwtTokenUtil jwtTokenUtil;
 
+    @GetMapping("/authorized/get_orders")
+    public ResponseEntity<List<OrderResponseDto>> getOrders(HttpServletRequest request) {
+        User user = getUserFromToken(request);
+        return ResponseEntity.ok(
+                userService.getOrders(user).stream()
+                        .map((order) -> OrderMapper.orderEntityToDto(order, orderService.getReview(order.getId())))
+                        .collect(Collectors.toList())
+        );
+    }
 
+    private User getUserFromToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer")) {
+            String token = authorizationHeader.substring(7);
+            String email = jwtTokenUtil.getUserNameFromToken(token);
+            return userService.findUserByEmail(email).get();
+        }
+        throw new RuntimeException();
+    }
 }
--- src/main/java/by/bsuir/fooddelivio/service/UserService.java
+++ src/main/java/by/bsuir/fooddelivio/service/UserService.java
@@ -1,12 +1,11 @@
 package by.bsuir.fooddelivio.service;
 
-import by.bsuir.fooddelivio.dto.LoginUserDto;
 import by.bsuir.fooddelivio.dto.RegisterUserDto;
+import by.bsuir.fooddelivio.entity.Order;
 import by.bsuir.fooddelivio.entity.User;
 import by.bsuir.fooddelivio.exception.UserAlreadyExistsException;
-import by.bsuir.fooddelivio.exception.UserDoesntExistsException;
-import by.bsuir.fooddelivio.exception.UserWrongPasswordException;
 import by.bsuir.fooddelivio.mapper.UserMapper;
+import by.bsuir.fooddelivio.repository.OrderRepository;
 import by.bsuir.fooddelivio.repository.RoleRepository;
 import by.bsuir.fooddelivio.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +15,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,9 +24,7 @@ import java.util.Optional;
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-    public Optional<User> findUserById(Long userId) {
-        return userRepository.findById(userId);
-    }
+
 
     public Optional<User> findUserByPhone(String phone) {
         return userRepository.findUserByPhoneNumber(phone);
@@ -51,16 +47,8 @@ public class UserService implements UserDetailsService {
         return createdUser;
     }
 
-    public User loginUser(LoginUserDto dto) throws UserDoesntExistsException, UserWrongPasswordException {
-        Optional<User> user = findUserByEmail(dto.getLogin());
-        user = user.isPresent() ? user : findUserByPhone(dto.getLogin());
-        if (user.isEmpty()) {
-            throw new UserDoesntExistsException();
-        }
-        if (user.get().getPassword().equals(dto.getPassword())) {
-            return user.get();
-        }
-        throw new UserWrongPasswordException();
+    public List<Order> getOrders(User user) {
+        return user.getOrders();
     }
 
     @Override
@@ -73,4 +61,6 @@ public class UserService implements UserDetailsService {
 
         return userFromDb.get();
     }
+
+
 }
